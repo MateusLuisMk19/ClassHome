4ec33b7e87c02f2c743622939f621bd47d34f99d
MateusLuisMk19 <95911492+MateusLuisMk19@users.noreply.github.com>
2022-02-06 00:39:06 +0000

    Eliminação
    Pasta Settings
    pasta Service

--- Services/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace ClassHome.Services
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string emailDestinatario, string assunto, string mensagemTexto, string mensagemHtml);
-    }
-}
